c3449b9d278791d91ad108bd06c28cc2ccc9ab31
Yash Saini <yyashssaini.20@gmail.com>
2021-08-27 01:08:59 +0530

    Add files via upload

---
+++ js/donate.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+    $('.items').slick({
+        dots: true,
+      
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+  autoplaySpeed: 2000,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          
+        ]
+      });
+    });
+
+
+    
+
+
+
+
+
+
+
+
+
+
